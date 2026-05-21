908cda76e84a8c5fb391dab7360baa8115ea25e5
BelovAl22 <alexanderbeldevan@gmail.com>
2026-05-22 01:00:53 +0700

    lab7suda

---
+++ lab07/IMlab7.2/IMlab7.2/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace IMlab7._2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        // Элементы управления
+        private System.Windows.Forms.DataGridView dgvQ;
+        private System.Windows.Forms.Button btnStart, btnStop, btnReset;
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.TrackBar trackDelay;
+        private System.Windows.Forms.RadioButton rbEvents, rbDays;
+        private System.Windows.Forms.NumericUpDown numLimit;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartPie;
+        private System.Windows.Forms.Label lblStatus, lblTime, lblDelay, lblMode;
+        private System.Windows.Forms.DataGridView dgvStats;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dgvQ = new System.Windows.Forms.DataGridView();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.trackDelay = new System.Windows.Forms.TrackBar();
+            this.rbEvents = new System.Windows.Forms.RadioButton();
+            this.rbDays = new System.Windows.Forms.RadioButton();
+            this.numLimit = new System.Windows.Forms.NumericUpDown();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.chartPie = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblDelay = new System.Windows.Forms.Label();
+            this.lblMode = new System.Windows.Forms.Label();
+            this.dgvStats = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvQ)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackDelay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numLimit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartPie)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStats)).BeginInit();
+            this.SuspendLayout();
+
+            // Матрица Q
+            this.dgvQ.AllowUserToAddRows = false;
+            this.dgvQ.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvQ.Location = new System.Drawing.Point(12, 12);
+            this.dgvQ.Size = new System.Drawing.Size(480, 115);
+
+            // Режим (текст)
+            this.lblMode.Location = new System.Drawing.Point(12, 140);
+            this.lblMode.Text = "Режим остановки:";
+            this.lblMode.Size = new System.Drawing.Size(150, 20);
+
+            // Радиокнопки
+            this.rbEvents.Checked = true;
+            this.rbEvents.Location = new System.Drawing.Point(15, 160);
+            this.rbEvents.Text = "По событиям (N)";
+            this.rbEvents.Size = new System.Drawing.Size(130, 24);
+
+            this.rbDays.Location = new System.Drawing.Point(150, 160);
+            this.rbDays.Text = "По дням (T)";
+            this.rbDays.Size = new System.Drawing.Size(130, 24);
+
+            // Ввод N или T
+            this.numLimit.Location = new System.Drawing.Point(15, 190);
+            this.numLimit.Maximum = new decimal(new int[] { 100000, 0, 0, 0 });
+            this.numLimit.Value = 1000;
+            this.numLimit.Size = new System.Drawing.Size(120, 20);
+
+            // Слайдер задержки (Выровнен с выбором дней)
+            this.trackDelay.Location = new System.Drawing.Point(145, 190);
+            this.trackDelay.Maximum = 1000;
+            this.trackDelay.Minimum = 10;
+            this.trackDelay.Value = 200;
+            this.trackDelay.Size = new System.Drawing.Size(250, 45);
+            this.trackDelay.Scroll += (s, e) => lblDelay.Text = trackDelay.Value + " мс";
+
+            this.lblDelay.Location = new System.Drawing.Point(400, 195);
+            this.lblDelay.Text = "200 мс";
+            this.lblDelay.Size = new System.Drawing.Size(60, 20);
+
+            // Кнопки
+            this.btnStart.BackColor = System.Drawing.Color.LightGreen;
+            this.btnStart.Location = new System.Drawing.Point(12, 250);
+            this.btnStart.Size = new System.Drawing.Size(120, 45);
+            this.btnStart.Text = "СТАРТ";
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+
+            this.btnStop.BackColor = System.Drawing.Color.LightCoral;
+            this.btnStop.Location = new System.Drawing.Point(140, 250);
+            this.btnStop.Size = new System.Drawing.Size(120, 45);
+            this.btnStop.Text = "СТОП";
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+
+            this.btnReset.Location = new System.Drawing.Point(270, 250);
+            this.btnReset.Size = new System.Drawing.Size(120, 45);
+            this.btnReset.Text = "СБРОС";
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+
+            // Лог
+            this.txtLog.Font = new System.Drawing.Font("Consolas", 9F);
+            this.txtLog.Location = new System.Drawing.Point(12, 310);
+            this.txtLog.Size = new System.Drawing.Size(480, 240);
+
+            // График
+            chartArea1.Name = "ChartArea1";
+            this.chartPie.ChartAreas.Add(chartArea1);
+            this.chartPie.Location = new System.Drawing.Point(510, 12);
+            this.chartPie.Size = new System.Drawing.Size(400, 280);
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.Name = "States";
+            this.chartPie.Series.Add(series1);
+
+            // Индикаторы
+            this.lblStatus.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblStatus.Location = new System.Drawing.Point(510, 305);
+            this.lblStatus.Size = new System.Drawing.Size(400, 25);
+            this.lblStatus.Text = "Событие: 0/0";
+
+            this.lblTime.Location = new System.Drawing.Point(510, 330);
+            this.lblTime.Size = new System.Drawing.Size(400, 20);
+            this.lblTime.Text = "Время: 0.00 дней";
+
+            this.progressBar.Location = new System.Drawing.Point(510, 360);
+            this.progressBar.Size = new System.Drawing.Size(380, 20);
+
+            // Статистика
+            this.dgvStats.AllowUserToAddRows = false;
+            this.dgvStats.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvStats.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStats.Location = new System.Drawing.Point(510, 395);
+            this.dgvStats.Size = new System.Drawing.Size(380, 155);
+
+            // Форма
+            this.ClientSize = new System.Drawing.Size(920, 565);
+            this.Controls.AddRange(new System.Windows.Forms.Control[] {
+                this.dgvQ, this.btnStart, this.btnStop, this.btnReset, this.txtLog,
+                this.trackDelay, this.rbEvents, this.rbDays, this.numLimit, this.progressBar,
+                this.chartPie, this.lblStatus, this.lblTime, this.lblDelay, this.lblMode, this.dgvStats
+            });
+            this.Text = "Марковская модель погоды (СТМС)";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvQ)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackDelay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numLimit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartPie)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStats)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ lab07/IMlab7.2/IMlab7.2/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMlab7._2
+{
+    public partial class Form1 : Form
+    {
+        private double[,] Q = new double[3, 3];
+        private double[] stateTimes = new double[3];
+        private double[] theoreticalProbs = new double[3];
+        private Random rnd = new Random();
+        private bool running = false;
+        private int currentState = 0;
+        private double totalTime = 0;
+        private int eventCount = 0;
+
+        private string[] stateNames = { "Ясно", "Облачно", "Пасмурно" };
+        private Color[] stateColors = { Color.Gold, Color.Gray, Color.DarkGray };
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupGrids();
+            InitMatrix();
+        }
+
+        private void SetupGrids()
+        {
+            dgvQ.ColumnCount = 3;
+            dgvQ.RowCount = 3;
+            for (int i = 0; i < 3; i++)
+            {
+                dgvQ.Columns[i].HeaderText = "→ " + stateNames[i];
+                dgvQ.Rows[i].HeaderCell.Value = "Из " + stateNames[i];
+                dgvQ.Columns[i].Width = 80;
+            }
+            dgvQ.RowHeadersWidth = 110;
+
+            dgvStats.ColumnCount = 4;
+            dgvStats.Columns[0].HeaderText = "Тип";
+            dgvStats.Columns[1].HeaderText = "Эмп.";
+            dgvStats.Columns[2].HeaderText = "Теор.";
+            dgvStats.Columns[3].HeaderText = "Разн.";
+            dgvStats.RowHeadersVisible = false;
+            dgvStats.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void InitMatrix()
+        {
+            dgvQ[0, 0].Value = -0.5; dgvQ[1, 0].Value = 0.3; dgvQ[2, 0].Value = 0.2;
+            dgvQ[0, 1].Value = 0.1; dgvQ[1, 1].Value = -0.4; dgvQ[2, 1].Value = 0.3;
+            dgvQ[0, 2].Value = 0.2; dgvQ[1, 2].Value = 0.1; dgvQ[2, 2].Value = -0.3;
+        }
+
+        private double SafeDouble(object val)
+        {
+            if (val == null) return 0;
+            string s = val.ToString().Replace(',', '.');
+            return double.Parse(s, CultureInfo.InvariantCulture);
+        }
+
+        private void CalculateTheoretical()
+        {
+            try
+            {
+                double[,] a = new double[3, 3];
+                for (int i = 0; i < 3; i++)
+                    for (int j = 0; j < 2; j++)
+                        a[j, i] = Q[i, j];
+
+                a[2, 0] = 1; a[2, 1] = 1; a[2, 2] = 1;
+                double[] b = { 0, 0, 1 };
+
+                double det = Det3x3(a);
+                if (Math.Abs(det) < 1e-9) return;
+
+                for (int i = 0; i < 3; i++)
+                {
+                    double[,] temp = (double[,])a.Clone();
+                    for (int j = 0; j < 3; j++) temp[j, i] = b[j];
+                    theoreticalProbs[i] = Det3x3(temp) / det;
+                }
+            }
+            catch { /* Игнорируем ошибки расчета Pi */ }
+        }
+
+        private double Det3x3(double[,] m) =>
+            m[0, 0] * (m[1, 1] * m[2, 2] - m[1, 2] * m[2, 1]) - m[0, 1] * (m[1, 0] * m[2, 2] - m[1, 2] * m[2, 0]) + m[0, 2] * (m[1, 0] * m[2, 1] - m[1, 1] * m[2, 0]);
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            if (running) return;
+            try
+            {
+                // Считываем матрицу правильно: Row i, Col j
+                for (int i = 0; i < 3; i++)
+                    for (int j = 0; j < 3; j++)
+                        Q[i, j] = SafeDouble(dgvQ[j, i].Value);
+            }
+            catch { MessageBox.Show("Проверьте формат чисел в матрице!"); return; }
+
+            CalculateTheoretical();
+            running = true;
+            double limit = (double)numLimit.Value;
+
+            while (running)
+            {
+                if (rbEvents.Checked && eventCount >= limit) break;
+                if (rbDays.Checked && totalTime >= limit) break;
+
+                double lambda = -Q[currentState, currentState];
+                if (lambda <= 0) break; // Защита от деления на 0
+
+                double dt = -Math.Log(rnd.NextDouble()) / lambda;
+
+                string timeStart = FormatTime(totalTime);
+                double oldTotal = totalTime;
+                totalTime += dt;
+
+                stateTimes[currentState] += dt;
+                eventCount++;
+
+                LogEvent(timeStart, FormatTime(totalTime), currentState, dt);
+                UpdateUI(limit);
+
+                currentState = GetNextState(currentState);
+                await Task.Delay(trackDelay.Value);
+            }
+
+            running = false;
+            if (eventCount > 0) ExportToCSV();
+        }
+
+        private string FormatTime(double days)
+        {
+            int d = (int)days;
+            int h = (int)((days - d) * 24);
+            int m = (int)((((days - d) * 24) - h) * 60);
+            return $"{d}д {h:D2}:{m:D2}";
+        }
+
+        private int GetNextState(int s)
+        {
+            double r = rnd.NextDouble();
+            double sum = 0;
+            double lambda = -Q[s, s];
+            for (int j = 0; j < 3; j++)
+            {
+                if (s == j) continue;
+                sum += Q[s, j] / lambda;
+                if (r <= sum) return j;
+            }
+            return s;
+        }
+
+        private void UpdateUI(double limit)
+        {
+            lblStatus.Text = $"Событие {eventCount}: {stateNames[currentState]}";
+            lblTime.Text = $"Время: {totalTime:F2} дней";
+
+            double progress = rbEvents.Checked ? (eventCount / limit * 100) : (totalTime / limit * 100);
+            progressBar.Value = (int)Math.Min(100, progress);
+
+            chartPie.Series[0].Points.Clear();
+            dgvStats.Rows.Clear();
+            for (int i = 0; i < 3; i++)
+            {
+                double emp = totalTime > 0 ? stateTimes[i] / totalTime : 0;
+                var p = chartPie.Series[0].Points.AddXY(stateNames[i], emp);
+                chartPie.Series[0].Points[i].Color = stateColors[i];
+                chartPie.Series[0].Points[i].Label = (i + 1).ToString();
+
+                dgvStats.Rows.Add(stateNames[i], $"{emp:F3}", $"{theoreticalProbs[i]:F3}", $"{Math.Abs(emp - theoreticalProbs[i]):F3}");
+            }
+        }
+
+        private void LogEvent(string start, string end, int state, double dur)
+        {
+            txtLog.SelectionStart = txtLog.TextLength;
+            txtLog.SelectionColor = Color.Orange;
+            txtLog.AppendText($"[{start}-{end}] ");
+
+            txtLog.SelectionColor = Color.Black;
+            txtLog.AppendText($"{stateNames[state].PadRight(10)} | ");
+
+            txtLog.SelectionColor = Color.Green;
+            txtLog.AppendText($"{FormatDuration(dur)}\n");
+            txtLog.ScrollToCaret();
+        }
+
+        private string FormatDuration(double d) => $"{(int)d}д {(int)((d - (int)d) * 24)}ч";
+
+        private void btnStop_Click(object sender, EventArgs e) => running = false;
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            running = false;
+            totalTime = 0; eventCount = 0; currentState = 0;
+            Array.Clear(stateTimes, 0, 3);
+            txtLog.Clear();
+            progressBar.Value = 0;
+            chartPie.Series[0].Points.Clear();
+            dgvStats.Rows.Clear();
+            lblStatus.Text = "Событие: 0/0";
+            lblTime.Text = "Время: 0.00 дней";
+        }
+
+        private void ExportToCSV()
+        {
+            try
+            {
+                var lines = new List<string> { "State;Empirical;Theoretical;Diff" };
+                for (int i = 0; i < 3; i++)
+                {
+                    double emp = stateTimes[i] / totalTime;
+                    lines.Add($"{stateNames[i]};{emp:F4};{theoreticalProbs[i]:F4};{Math.Abs(emp - theoreticalProbs[i]):F4}");
+                }
+                File.WriteAllLines("weather_results.csv", lines);
+            }
+            catch { }
+        }
+    }
+}
